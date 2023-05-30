eea17b7f15255c7b3161537406460adb07ef8c14
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-30 08:52:50 +0545

    day6

---
+++ notesapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { motion } from "framer-motion"
+
+function App() {
+  
+
+  return (
+    <>
+      <motion.div animate={{y=}}>
+
+      </motion.div>
+    </>
+  )
+}
+
+export default App
