09bd082f0c1b24e3d24c541fb892b3df8e23374b
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-06-01 09:25:10 +0545

    day8

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -20,9 +20,9 @@ function App() {
     const targetTimestamp = targetDate.getTime();
     const millisecondsInHour = 1000 * 60 * 60;
     const hoursUntilTarget = Math.floor((targetTimestamp - currentTimestamp) / millisecondsInHour);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apikey}`)
     .then(res=>res.json())
-    .then(data=>setDayData(data))
+    .then(data=>setDayData([data]))
     fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${cityName}&units=metric&cnt=${hoursUntilTarget}&appid=${apikey}`)
       .then(res => res.json())
       .then(data => {
@@ -39,7 +39,7 @@ return (
     handleChange={handleChange}
     handleClick={handleClick}/>
     <Day
-    dayData={dayData}/>
+    dayData={dayData[0]}/>
     <div 
     className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
       {cards}
--- weatherapp/src/components/Day.jsx
+++ weatherapp/src/components/Day.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function Day(props){
+export default function Day({dayData}){
+    const {dt, weather, name , main }=dayData
     return (
         <div
         className="w-1/2 mx-auto p-2 m-5 border border-solid border-black rounded-xl">
-            <h1>{props.dayData.dt}</h1>
-            <h1>{props.dayData.weather.icon}</h1>
+            <h1>{name}</h1>
+            <h1>{dt}</h1>
+            <h1>{weather[0].main}</h1>
+            <h1>{main.temp}</h1>
         </div>
     );
 }
